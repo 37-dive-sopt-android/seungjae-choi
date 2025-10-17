d748e175d0698bd75884c04a364df7fa0f8b2d23
seungjae708 <choesj365@gmail.com>
2025-10-17 16:34:45 +0900

    :sparkles: [FEAT] (#2) MainActivity.kt 화면 구현

--- app/src/main/java/com/sopt/dive/MainActivity.kt
+++ app/src/main/java/com/sopt/dive/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sopt.dive.ui.theme.DiveTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DiveTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +44,119 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainScreen(modifier: Modifier = Modifier) {
+    val idValue = "idValue"
+    val pwValue = "pwValue"
+    val nameValue = "nameValue"
+    val nicknameValue = "nicknameValue"
+    val mbtiValue = "mbtiValue"
+
+    Column(
         modifier = modifier
-    )
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+            )
+
+            Text(
+                text = nameValue,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+            )
+        }
+
+        Text(
+            text = "안녕하세요. 안드로이드 YB ${nameValue}입니다.",
+            modifier = Modifier
+                .padding(top = 12.dp)
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "ID",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+            Text(
+                text = idValue,
+                fontSize = 18.sp,
+                color = Color.Gray
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "PW",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(bottom = 4.dp)
+            )
+
+            Text(
+                text = pwValue,
+                fontSize = 18.sp,
+                color = Color.Gray
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "NICKNAME",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+            Text(
+                text = nicknameValue,
+                fontSize = 18.sp,
+                color = Color.Gray
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "MBTI",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+            Text(
+                text = mbtiValue,
+                fontSize = 18.sp,
+                color = Color.Gray
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     DiveTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
