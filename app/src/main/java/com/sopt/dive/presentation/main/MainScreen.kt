2ba19e62d81435add1d883903b72427494e4bc95
seungjae708 <choesj365@gmail.com>
2025-10-31 15:46:01 +0900

    :recycle: [REFACTOR] (#4) 중복으로 패딩 적용되던 문제 수정

--- app/src/main/java/com/sopt/dive/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sopt/dive/presentation/main/MainScreen.kt
@@ -1,7 +1,6 @@
 package com.sopt.dive.presentation.main
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -92,7 +91,6 @@ fun MainScreen(
             startDestination = startDestination,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
         ) {
             signInNavGraph(
                 navigateToSignUp = navController::navigateToSignUp,
