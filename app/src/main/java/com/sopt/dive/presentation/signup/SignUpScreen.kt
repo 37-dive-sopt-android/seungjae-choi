0665a0196ede06d373e1976dafbc125d12ab6f1b
seungjae708 <choesj365@gmail.com>
2025-10-31 14:22:45 +0900

    :sparkles: [FEAT] (#4) SAA 구조로 수정

--- app/src/main/java/com/sopt/dive/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/sopt/dive/presentation/signup/SignUpScreen.kt
@@ -1,10 +1,5 @@
-package com.sopt.dive
+package com.sopt.dive.presentation.signup
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,7 +11,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -36,6 +30,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sopt.dive.core.data.UserData
 import com.sopt.dive.core.data.UserManager
 import com.sopt.dive.core.designsystem.component.SoptButton
 import com.sopt.dive.core.designsystem.component.SoptFormField
@@ -43,50 +38,54 @@ import com.sopt.dive.core.designsystem.theme.DiveTheme
 import com.sopt.dive.core.extention.showToast
 import com.sopt.dive.core.util.Validator
 
-class SignUpActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            DiveTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SignUpScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        onSignUpComplete = { id, pw, name, nickname, mbti ->
-                            val resultIntent = Intent().apply {
-                                putExtra(UserManager.SharedPrefKeys.USER_ID, id)
-                                putExtra(UserManager.SharedPrefKeys.USER_PASSWORD, pw)
-                                putExtra(UserManager.SharedPrefKeys.USER_NAME, name)
-                                putExtra(UserManager.SharedPrefKeys.USER_NICKNAME, nickname)
-                                putExtra(UserManager.SharedPrefKeys.USER_MBTI, mbti)
-                            }
-                            setResult(RESULT_OK, resultIntent)
-                            finish()
-                        })
-                }
-            }
-        }
-    }
-}
-
 @Composable
-fun SignUpScreen(
-    modifier: Modifier = Modifier,
-    onSignUpComplete: (String, String, String, String, String) -> Unit
+fun SignUpRoute(
+    onSignUpComplete: () -> Unit
 ) {
+    val context = LocalContext.current
+    val userManager = remember { UserManager(context) }
+
     var id by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var name by remember { mutableStateOf("") }
     var nickname by remember { mutableStateOf("") }
     var mbti by remember { mutableStateOf("") }
 
+    SignUpScreen(
+        id = id, onIdChange = { id = it },
+        password = password, onPasswordChange = { password = it },
+        name = name, onNameChange = { name = it },
+        nickname = nickname, onNicknameChange = { nickname = it },
+        mbti = mbti, onMbtiChange = { mbti = it },
+        onSignUpClick = {
+            val errorMessage = Validator.validateSignUpInputs(id, password, name, nickname, mbti)
+            if (errorMessage != null) {
+                context.showToast(errorMessage)
+            } else {
+                userManager.saveUserData(UserData(id, password, name, nickname, mbti))
+                context.showToast("회원가입에 성공했습니다!")
+
+                onSignUpComplete()
+            }
+        }
+    )
+}
+
+@Composable
+private fun SignUpScreen(
+    modifier: Modifier = Modifier,
+    id: String, onIdChange: (String) -> Unit,
+    password: String, onPasswordChange: (String) -> Unit,
+    name: String, onNameChange: (String) -> Unit,
+    nickname: String, onNicknameChange: (String) -> Unit,
+    mbti: String, onMbtiChange: (String) -> Unit,
+    onSignUpClick: () -> Unit
+) {
     val focusRequesterPassword = remember { FocusRequester() }
     val focusRequesterName = remember { FocusRequester() }
     val focusRequesterNickname = remember { FocusRequester() }
     val focusRequesterMbti = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
-
-    val context = LocalContext.current
 
     Column(
         modifier = modifier
@@ -111,86 +110,58 @@ fun SignUpScreen(
             verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
             SoptFormField(
-                label = "ID",
-                value = id,
-                onValueChange = { id = it },
+                label = "ID", value = id, onValueChange = onIdChange,
                 placeholder = "아이디를 입력해주세요.",
                 maxLines = 1,
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Next
                 ),
-                keyboardActions = KeyboardActions(
-                    onNext = { focusRequesterPassword.requestFocus() }
-                ),
+                keyboardActions = KeyboardActions(onNext = { focusRequesterPassword.requestFocus() }),
             )
-
             SoptFormField(
-                label = "PW",
-                value = password,
-                onValueChange = { password = it },
+                label = "PW", value = password, onValueChange = onPasswordChange,
                 placeholder = "비밀번호를 입력해주세요.",
                 maxLines = 1,
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Next
                 ),
-                keyboardActions = KeyboardActions(
-                    onNext = { focusRequesterName.requestFocus() }
-                ),
-                textFieldModifier = Modifier
-                    .focusRequester(focusRequesterPassword)
+                keyboardActions = KeyboardActions(onNext = { focusRequesterName.requestFocus() }),
+                textFieldModifier = Modifier.focusRequester(focusRequesterPassword)
             )
-
             SoptFormField(
-                label = "NAME",
-                value = name,
-                onValueChange = { name = it },
+                label = "NAME", value = name, onValueChange = onNameChange,
                 placeholder = "이름을 입력해주세요.",
                 maxLines = 1,
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Next
                 ),
-                keyboardActions = KeyboardActions(
-                    onNext = { focusRequesterNickname.requestFocus() }
-                ),
-                textFieldModifier = Modifier
-                    .focusRequester(focusRequesterName)
+                keyboardActions = KeyboardActions(onNext = { focusRequesterNickname.requestFocus() }),
+                textFieldModifier = Modifier.focusRequester(focusRequesterName)
             )
-
             SoptFormField(
-                label = "NICKNAME",
-                value = nickname,
-                onValueChange = { nickname = it },
+                label = "NICKNAME", value = nickname, onValueChange = onNicknameChange,
                 placeholder = "닉네임을 입력해주세요.",
                 maxLines = 1,
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Text,
                     imeAction = ImeAction.Next
                 ),
-                keyboardActions = KeyboardActions(
-                    onNext = { focusRequesterMbti.requestFocus() }
-                ),
-                textFieldModifier = Modifier
-                    .focusRequester(focusRequesterNickname)
+                keyboardActions = KeyboardActions(onNext = { focusRequesterMbti.requestFocus() }),
+                textFieldModifier = Modifier.focusRequester(focusRequesterNickname)
             )
-
             SoptFormField(
-                label = "MBTI",
-                value = mbti,
-                onValueChange = { mbti = it },
+                label = "MBTI", value = mbti, onValueChange = onMbtiChange,
                 placeholder = "MBTI를 입력해주세요.",
                 maxLines = 1,
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Ascii,
                     imeAction = ImeAction.Done
                 ),
-                keyboardActions = KeyboardActions(
-                    onDone = { focusManager.clearFocus() }
-                ),
-                textFieldModifier = Modifier
-                    .focusRequester(focusRequesterMbti)
+                keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+                textFieldModifier = Modifier.focusRequester(focusRequesterMbti)
             )
         }
 
@@ -198,18 +169,7 @@ fun SignUpScreen(
 
         SoptButton(
             label = "회원가입 하기",
-            onClick = {
-                val errorMessage = Validator.validateSignUpInputs(
-                    id, password, name, nickname, mbti
-                )
-
-                if (errorMessage != null) {
-                    context.showToast(errorMessage)
-                } else {
-                    context.showToast("회원가입에 성공했습니다!")
-                    onSignUpComplete(id, password, name, nickname, mbti)
-                }
-            },
+            onClick = onSignUpClick,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 24.dp, bottom = 24.dp)
@@ -221,5 +181,19 @@ fun SignUpScreen(
 @Preview(showBackground = true)
 @Composable
 private fun SignUpScreenPreview() {
-    SignUpScreen(onSignUpComplete = { _, _, _, _, _ -> })
+    DiveTheme {
+        SignUpScreen(
+            id = "",
+            onIdChange = {},
+            password = "",
+            onPasswordChange = {},
+            name = "",
+            onNameChange = {},
+            nickname = "",
+            onNicknameChange = {},
+            mbti = "",
+            onMbtiChange = {},
+            onSignUpClick = {}
+        )
+    }
 }
