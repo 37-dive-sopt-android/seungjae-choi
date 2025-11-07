309d455f2c5a1a704da43fb277ae2b4bc55d0974
seungjae708 <choesj365@gmail.com>
2025-11-07 18:03:58 +0900

    :sparkles: [FEAT] (#6) @Immutable 어노테이션을 적용

--- app/src/main/java/com/sopt/dive/presentation/home/model/HomeData.kt
+++ app/src/main/java/com/sopt/dive/presentation/home/model/HomeData.kt
@@ -1,17 +1,22 @@
 package com.sopt.dive.presentation.home.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 sealed class FriendAction {
     data object None : FriendAction()
     data class Music(val title: String) : FriendAction()
     data object Gift : FriendAction()
 }
 
+@Immutable
 data class MyProfile(
     val name: String,
     val statusMessage: String,
     val profileImageRes: Int
 )
 
+@Immutable
 data class Friend(
     val id: Int,
     val name: String,
@@ -21,6 +26,7 @@ data class Friend(
     val action: FriendAction
 )
 
+@Immutable
 data class HomeUiState(
     val myProfile: MyProfile,
     val friendList: List<Friend>
