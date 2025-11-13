64082c14c7cca2528507e33b7bc3350f3ab32ed2
seungjae708 <choesj365@gmail.com>
2025-11-14 04:24:49 +0900

    :sparkles: [FEAT] (#8) UserRepository 구현

---
+++ app/src/main/java/com/sopt/dive/data/repository/UserRepository.kt
@@ -0,0 +1,16 @@
+package com.sopt.dive.data.repository
+
+import com.sopt.dive.data.dto.response.UserDetailResponseDto
+import com.sopt.dive.data.service.UserService
+
+class UserRepository(private val userService: UserService) {
+    suspend fun getUser(userId: Int): Result<UserDetailResponseDto> = runCatching {
+        val response = userService.getUser(userId)
+
+        if (response.success) {
+            response.data ?: throw IllegalStateException()
+        } else {
+            throw RuntimeException(response.message)
+        }
+    }
+}
