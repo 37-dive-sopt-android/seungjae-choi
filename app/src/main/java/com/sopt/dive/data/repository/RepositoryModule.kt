e61b949127f9cb814b12fd6ca041dfde840375e3
seungjae708 <choesj365@gmail.com>
2025-11-14 04:55:10 +0900

    :sparkles: [FEAT] (#8) RepositoryModule,ViewModelFactory에 추가

--- app/src/main/java/com/sopt/dive/data/repository/RepositoryModule.kt
+++ app/src/main/java/com/sopt/dive/data/repository/RepositoryModule.kt
@@ -2,11 +2,18 @@ package com.sopt.dive.data.repository
 
 import com.sopt.dive.data.remote.ServicePool
 import com.sopt.dive.data.service.AuthService
+import com.sopt.dive.data.service.UserService
 
 object RepositoryModule {
     private val authService: AuthService = ServicePool.authService
+    private val userService: UserService = ServicePool.userService
 
     val authRepository: AuthRepository by lazy {
         AuthRepository(authService)
     }
+
+    val userRepository: UserRepository by lazy {
+        UserRepository(userService)
+    }
+
 }
--- app/src/main/java/com/sopt/dive/presentation/common/ViewModelFactory.kt
+++ app/src/main/java/com/sopt/dive/presentation/common/ViewModelFactory.kt
@@ -5,11 +5,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.sopt.dive.data.local.UserManager
 import com.sopt.dive.data.repository.AuthRepository
+import com.sopt.dive.data.repository.UserRepository
+import com.sopt.dive.presentation.mypage.MyPageViewModel
 import com.sopt.dive.presentation.signin.SignInViewModel
 import com.sopt.dive.presentation.signup.SignUpViewModel
 
 class ViewModelFactory(
     private val authRepository: AuthRepository,
+    private val userRepository: UserRepository,
     private val context: Context
 ) : ViewModelProvider.Factory {
 
@@ -20,6 +23,9 @@ class ViewModelFactory(
             }
             modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
                 SignUpViewModel(authRepository, UserManager(context)) as T
+            }
+            modelClass.isAssignableFrom(MyPageViewModel::class.java) -> {
+                MyPageViewModel(UserManager(context), userRepository) as T
             }
 
             else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
