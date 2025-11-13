6397d672f53e1835d86e94df21a190082f004891
seungjae708 <choesj365@gmail.com>
2025-11-14 04:06:26 +0900

    :sparkles: [FEAT] (#8) AuthRepository 구현

---
+++ app/src/main/java/com/sopt/dive/data/repository/AuthRepository.kt
@@ -0,0 +1,29 @@
+package com.sopt.dive.data.repository
+
+import com.sopt.dive.data.dto.request.LogInRequestDto
+import com.sopt.dive.data.dto.request.RegisterRequestDto
+import com.sopt.dive.data.dto.response.LoginResponseDto
+import com.sopt.dive.data.dto.response.RegisterResponseDto
+import com.sopt.dive.data.service.AuthService
+
+class AuthRepository(private val authService: AuthService) {
+    suspend fun register(request: RegisterRequestDto): Result<RegisterResponseDto> = runCatching {
+        val response = authService.register(request)
+
+        if (response.success) {
+            response.data ?: throw IllegalStateException()
+        } else {
+            throw RuntimeException(response.message)
+        }
+    }
+
+    suspend fun login(request: LogInRequestDto): Result<LoginResponseDto> = runCatching {
+        val response = authService.login(request)
+
+        if (response.success) {
+            response.data ?: throw IllegalStateException()
+        } else {
+            throw RuntimeException(response.message)
+        }
+    }
+}
